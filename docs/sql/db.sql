1e98cd1cd9e6f08864bd205483faee9389171afb
Alexis Massa <63648046+massa-alexis@users.noreply.github.com>
2020-12-17 09:59:07 +0100

    gf

--- docs/sql/db.sql
+++ docs/sql/db.sql
@@ -51,3 +51,229 @@ create table rencontre(
 --/!\ Procédure initrencontres /!\
 --Les rencontres commencent au 7 mars 2021
 select * from initRencontres('07-04-2021');
+
+
+update rencontre set arbitre = 'Mr L''arbitre' where 1 = 1;
+
+--Create score
+create table score (
+	id_equipe int,
+	played int,
+	points int,
+	won int,
+	draw int,
+	lost int,
+	scored int,
+	taken int,
+	constraint pk_score primary key(id_equipe),
+	constraint fk_equipe foreign key(id_equipe) references equipe(id_equipe)
+);
+
+--Insert score
+insert into score values(1, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(2, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(4, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(5, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(6, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(8, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(10, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(11, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(13, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(15, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(16, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(17, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(19, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(30, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(700, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(125, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(456, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(354, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(854, 0, 0, 0, 0, 0, 0, 0);
+insert into score values(4356, 0, 0, 0, 0, 0, 0, 0);
+
+--Ajout direct
+create or replace function ajouter_equipe(p_nom varchar(50), p_stade varchar(50), p_ville varchar(50)) returns void  as $$
+DECLARE
+	query1 text;
+BEGIN
+	query1 := 'insert into equipe(nom, stade, ville, points, buts_pour, buts_contre) values(%, %, %, 0, 0, 0)', p_nom, p_stade, p_ville;
+	execute query1;
+END;
+$$ language plpgsql;
+
+--Ajout avec vérifications : déjà 22 équipes / saison débutée
+--/!\ Utilise ajouter_equipe() /!\
+create or replace function ajout_equipe(p_nom varchar(50), p_stade varchar(50), p_ville varchar(50)) returns void  as $$
+DECLARE
+	nb_equipe int;
+	nb_passee int;
+	query1 text;
+BEGIN
+	query1 := 'select * from ajouter_equipe(%, %, %)', p_nom, p_stade, p_ville;
+	
+	select count(*)
+	into nb_equipe
+	from equipe;
+	
+	select count(*)
+	into nb_passee
+	from rencontre
+	where etat = 'passée';
+	
+	if nb_equipe = 22 then
+		raise notice 'Il y a déjà 22 équipes !';
+	elsif nb_passee > 0 then
+		raise notice 'La saison est déjà débutée !';
+	else
+		execute query1;
+	end if;
+END;
+$$ language plpgsql;
+
+create or replace function initRencontres(firstDay date) returns void as $$
+declare
+	arrEq int array;
+	i int;
+	j int;
+	tmp int;
+	nbDecal int;
+	eqA int;
+	eqB int;
+	stmtRencontre text;
+	stmt text;
+begin
+	arrEq := array(
+	select id_equipe
+	from equipe
+	order by random());
+	
+	stmt:='';
+	stmtRencontre:='';
+	nbDecal:=1;
+	--raise notice '%', arrEq;
+	--BOUCLE DECALAGE
+	while nbDecal < array_length(arrEq, 1) loop
+		--raise notice '----------------- % --------------------', nbDecal;
+		i:=1;
+		j:= array_length(arrEq,1);
+		while i < j loop
+			while i < array_length(arrEq, 1) loop		
+				eqA := arrEq[i];
+				eqB := arrEq[j];
+				--raise notice '%, %, %', eqA, eqB, firstDay;
+				stmtRencontre := 'insert into rencontre (id_domicile, id_visiteur, date_match,score_domicile, score_visiteur, arbitre, etat) values (' || eqA || ',' || eqB || ',''' ||  firstDay || ''', 0, 0, '''', ''à venir'');';
+				--raise notice '%', stmtRencontre;
+				execute stmtRencontre;
+				stmt := stmt || chr(10) || stmtRencontre;
+				i:=i+1;
+				j:=j-1;
+			end loop;
+		end loop;
+		--DECALAGE
+		tmp := arrEq[array_length(arrEq,1)];
+		for i in reverse array_length(arrEq, 1)..2 loop
+			arrEq[i] := arrEq[i-1];
+		end loop;
+		arrEq[2] := tmp;
+		--raise notice '%', arrEq;
+		
+		nbDecal:=nbDecal+1;
+		firstDay := firstDay+7;
+	end loop;
+	--REDEMMARAGE + FIN BOUCLE DECALAGE
+	raise notice '%', stmt;
+	execute stmt;
+	
+end
+$$ language plpgsql;
+
+create or replace function resetSaison() returns void  as $$
+DECLARE 
+  v_tab_equipe text;
+  v_tab_rencontre text;
+  query1 text;
+  query2 text;
+  query3 text;
+  query4 text
+ BEGIN
+ 	SELECT Concat('save_', 'equipe_', date_part('year', now())) 
+ 	INTO   v_tab_equipe
+ 	FROM   equipe;
+	SELECT Concat('save_', 'rencontre_', date_part('year', now())) 
+ 	INTO   v_tab_rencontre
+ 	FROM   rencontre;
+	SELECT Concat('save_', 'score_', date_part('year', now())) 
+ 	INTO   v_tab_rencontre
+ 	FROM   rencontre;
+	query1 := format('create table if not exists %I as (select * from equipe)', lower(v_tab_equipe));
+	query2 := format('create table if not exists %I as (select * from rencontre)', lower(v_tab_rencontre));
+	query3 := format('create table if not exists %I as (select * from score)', lower(v_tab_rencontre));
+	query4 := format('truncate equipe, rencontre, score');
+	EXECUTE query1;
+	EXECUTE query2;
+	EXECUTE query4;
+	EXECUTE query3;
+  end;
+$$ language plpgsql;
+
+create or replace function saisonTerminee() returns void  as $$
+DECLARE
+	termine int;
+	nb_rencontre int;
+	query text;
+BEGIN
+
+	select count(*)
+	into nb_rencontre
+	from rencontre;
+	
+	select count(*)
+	into termine
+	from rencontre
+	where etat = 'passée';	
+	
+	query := 'select * from resetsaison()';
+		
+	if termine <> nb_rencontre then
+		raise notice 'saison non terminée !';
+	elsif termine = nb_rencontre then
+		raise notice 'saison terminée !';
+		execute (query);
+	end if;
+END;
+$$ language plpgsql;
+
+create or replace function updateScore() returns void as $$
+declare
+    i record;
+    vis int;
+    dom int;
+    score1 int;
+    score2 int;
+    stmt text;
+begin
+	execute 'update score set played=0, points=0, won=0, draw=0, lost=0, scored=0, taken=0 where 1=1;';
+    for i in select * from rencontre where etat = 'passé' loop
+        select i.score_domicile into score1 from rencontre;
+        select i.score_visiteur into score2 from rencontre;
+        select i.id_domicile into dom from rencontre;
+        select i.id_visiteur into vis from rencontre;
+        if score1 > score2 then
+            --raise notice '% : dom > vis : %', dom, vis;
+            execute 'update score set points = points+3, won = won+1  where id_equipe = ' || dom || ';';
+            execute 'update score set lost = lost + 1 where id_equipe = ' || vis || ';';
+        elsif score1 < score2 then
+            --raise notice '% : dom < vis : %', dom, vis;
+            execute 'update score set points = points+3, won = won+1 where id_equipe = ' || vis || ';';
+            execute 'update score set lost = lost + 1 where id_equipe = ' || dom || ';';
+        elsif score1 = score2 then
+            --raise notice '% : dom = vis : %', dom, vis;
+            execute 'update score set points = points + 1, draw = draw+1 where id_equipe = ' || dom || ';';
+            execute 'update score set points = points + 1, draw = draw+1 where id_equipe = ' || vis || ';';
+        end if;
+        execute 'update score set played = played+1, scored = scored + '|| score1 ||', taken = taken+'||score2||' where id_equipe = ' || dom || ';';
+        execute 'update score set played = played+1, scored = scored + '|| score2 ||', taken = taken+'||score1||' where id_equipe = ' || vis || ';';
+    end loop;
+end;
+$$ language plpgsql;
+
--- docs/sql/resetSaison.sql
+++ docs/sql/resetSaison.sql
@@ -5,18 +5,24 @@ DECLARE
   query1 text;
   query2 text;
   query3 text;
+  query4 text
  BEGIN
  	SELECT Concat('save_', 'equipe_', date_part('year', now())) 
  	INTO   v_tab_equipe
  	FROM   equipe;
-	 SELECT Concat('save_', 'rencontre_', date_part('year', now())) 
+	SELECT Concat('save_', 'rencontre_', date_part('year', now())) 
+ 	INTO   v_tab_rencontre
+ 	FROM   rencontre;
+	SELECT Concat('save_', 'score_', date_part('year', now())) 
  	INTO   v_tab_rencontre
  	FROM   rencontre;
 	query1 := format('create table if not exists %I as (select * from equipe)', lower(v_tab_equipe));
 	query2 := format('create table if not exists %I as (select * from rencontre)', lower(v_tab_rencontre));
-	query3 := format('truncate equipe, rencontre');
+	query3 := format('create table if not exists %I as (select * from score)', lower(v_tab_rencontre));
+	query4 := format('truncate equipe, rencontre, score');
 	EXECUTE query1;
 	EXECUTE query2;
+	EXECUTE query4;
 	EXECUTE query3;
   end;
 $$ language plpgsql;
